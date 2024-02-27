d6cf4708b27c05dc8200624dab83e8d4a6de983f
enemy <nicolae.gheorghe01@stud.etti.upb.ro>
2024-02-28 01:22:06 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://crazy-trade-frontend.vercel.app',
     methods: ["POST", "GET"],
     credentials: true,
   })
 );
+
 
 app.options('*', cors());
 
