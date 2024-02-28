1a5350b87de500685cc75fdbacf97c7208494e93
enemy <nicolae.gheorghe01@stud.etti.upb.ro>
2024-02-28 23:28:47 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.use(cors({
+  origin: 'https://crazy-trade-frontend.vercel.app',
+  credentials: true,
+}));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
