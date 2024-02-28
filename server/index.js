01dd2fa36d82d37d02515c98405875d39061fec6
enemy <nicolae.gheorghe01@stud.etti.upb.ro>
2024-02-28 23:21:00 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,12 +5,6 @@ const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
-
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
